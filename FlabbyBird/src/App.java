03be81d0fed8fc7f0d2b4444198194141bb04466
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-03 20:37:02 -0500

    adding some imports

--- FlabbyBird/src/App.java
+++ FlabbyBird/src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocation(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
