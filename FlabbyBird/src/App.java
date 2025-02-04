0e8878e4b8ad6235c26891f9bb662fd1994244dd
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-03 21:25:29 -0500

    dont even remember

--- FlabbyBird/src/App.java
+++ FlabbyBird/src/App.java
@@ -7,11 +7,17 @@ public class App {
         int boardWidth = 360;
         int boardHeight = 640;
 
-        JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
+       JFrame frame = new JFrame("Flappy Bird");
+        //frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
-        frame.setLocation(null);
+        frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    FlappyBird flappyBird = new FlappyBird();
+     
+    frame.add(flappyBird);
+    frame.pack();
+    frame.setVisible(true);
     }
 }
