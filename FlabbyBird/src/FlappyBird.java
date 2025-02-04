8afe1468432657de6b283d266682d71dd8f270b0
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-03 21:25:50 -0500

    not much

--- FlabbyBird/src/FlappyBird.java
+++ FlabbyBird/src/FlappyBird.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird {
+public class FlappyBird extends JPanel {
 
-    
+    int boardWidth = 360;
+    int boardHeight = 640;
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+
+    }
 }
