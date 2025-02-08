5156ddd0013762e5d4c59b61c7871eecfdec7e47
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-07 22:35:01 -0500

    setting up server

--- chatApp/src/server.java
+++ chatApp/src/server.java
@@ -1,6 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class server implements Runnable {
     @Override
     public void run(){
+        try{
+        ServerSocket server = new ServerSocket(9999);
+        Socket client = server.accept();
+        } catch(IOException e){
+
+        }
+
+    }
+
+     class connectionHandle  implements Runnable {
+      
+        @Override
+        public void run(){
+    
+        }
         
     }
 
