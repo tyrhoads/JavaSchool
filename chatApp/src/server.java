28714eeb12060cb6e5322372d05c1329b3465419
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-07 23:00:19 -0500

    added a lot

--- chatApp/src/server.java
+++ chatApp/src/server.java
@@ -1,24 +1,66 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class server implements Runnable {
+   private ArrayList<connectionHandler> conections;
+   
     @Override
     public void run(){
         try{
         ServerSocket server = new ServerSocket(9999);
         Socket client = server.accept();
-        } catch(IOException e){
+        connectionHandler handler = new connectionHandler(client);
+                conections.add(handler);
+                } catch(IOException e){
+        
+                }
+        
+            }
+        
+            public void broadcast(String mesage){
 
+                for(connectionHandler ch : conections){
+                    if (ch != null) {
+                        ch.sendMessage(mesage);
+                    }
+                }
+        
+            }
+             class connectionHandler  implements Runnable {
+              
+                private  Socket client;
+                private BufferedReader in;
+                private PrintWriter out;
+                private String nickName;
+                public connectionHandler(Socket client2) {
+                    //TODO Auto-generated constructor stub
+                }
+        
+        
+                @Override
+        public void run(){
+            try{
+                out =  new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("please enter a nick name");
+                nickName = in.readLine();
+                System.out.println(nickName + " contected!");
+                broadcast(nickName + " joined the chat");
+            }catch(IOException e){
+
+            }
+    
         }
 
-    }
 
-     class connectionHandle  implements Runnable {
-      
-        @Override
-        public void run(){
-    
+        public void sendMessage(String mesage)
+        {
+            out.println(mesage);
         }
         
     }
