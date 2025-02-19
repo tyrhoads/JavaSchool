b12ff921551cb3cfa27b9cb7e66d32c2a893c18e
Tyler Rhoads <tyrhoads@gmail.com>
2025-02-18 19:31:38 -0500

    added

---
+++ assignment 2/src/main/java/edu/odu/cs/cs261/Decoration.java
@@ -0,0 +1,21 @@
+package edu.odu.cs.cs261;
+/**
+ * 
+ */
+
+/**
+ * A decorative object that hangs from the "terminal"
+ * points of the mobile. DecorativeWeights have the following
+ * properties:
+ *   1) They have a positive (non-zero) weight
+ *   2) They hang from a bar, but nothing hangs from them.
+ *   3) Their width is negligible compared to the bars, and so can
+ *         be treated as zero. 
+ *  
+ * @author zeil
+ *
+ */
+public class Decoration extends MobileComponent {
+
+
+ }
