0389c5b2620f4fce0690d9476c5a6af4e28e5039
Tyler Rhoads <tyrhoads@gmail.com>
2025-09-24 23:02:44 -0400

    got add unit test to pass

--- Fall-CS330-Inventory-Iterator-main/src/main/java/items/Inventory.java
+++ Fall-CS330-Inventory-Iterator-main/src/main/java/items/Inventory.java
@@ -94,8 +94,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
      */
     public boolean isFull()
     {
-        // Replace the next line
-        return false;
+        return this.emptySlots()==0;
     }
 
     /**
@@ -131,6 +130,7 @@ public class Inventory implements Iterable<ItemStack>, Cloneable
     public void addItemStackNoCheck(ItemStack toAdd)
     {
         // Add the missing (one) line by using `this.slots.add(????)`
+        this.slots.add(toAdd);
     }
 
     /**
