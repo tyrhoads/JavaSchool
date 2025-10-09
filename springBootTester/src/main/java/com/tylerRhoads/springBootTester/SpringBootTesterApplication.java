1a3d813f4fc5b0bbf54776755b2c1cd05e673a98
Tyler Rhoads <tyrhoads@gmail.com>
2025-10-08 22:58:15 -0400

    added

---
+++ springBootTester/src/main/java/com/tylerRhoads/springBootTester/SpringBootTesterApplication.java
@@ -0,0 +1,13 @@
+package com.tylerRhoads.springBootTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTesterApplication.class, args);
+	}
+
+}
