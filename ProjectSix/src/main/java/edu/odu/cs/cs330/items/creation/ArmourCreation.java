ccb373ddb6c9649f4d308b4d27c3f0681aaff7f0
Tyler Rhoads <tyrhoads@gmail.com>
2025-11-19 22:51:32 -0500

    finished

--- ProjectSix/src/main/java/edu/odu/cs/cs330/items/creation/ArmourCreation.java
+++ ProjectSix/src/main/java/edu/odu/cs/cs330/items/creation/ArmourCreation.java
@@ -29,7 +29,7 @@ public class ArmourCreation implements ItemCreationStrategy
     public int requiredNumberOfValues()
     {
         // What is the correct return value?
-        return -1;
+        return 7;
     }
 
     @SuppressWarnings({
@@ -41,7 +41,13 @@ public class ArmourCreation implements ItemCreationStrategy
     {
         Armour armour = new Armour();
 
-        // Call the appropriate setters...
+        armour.setName(tokens[0]);
+        armour.setMaterial(tokens[1]);
+        armour.setDurability(Integer.parseInt(tokens[2]));
+        armour.setDefense(Integer.parseInt(tokens[3]));
+        armour.setModifier(tokens[4]);
+        armour.setModifierLevel(Integer.parseInt(tokens[5]));
+        armour.setElement(tokens[6]);
 
         return armour;
     }
@@ -49,7 +55,17 @@ public class ArmourCreation implements ItemCreationStrategy
     @Override
     public Item fromExisting(final Item original)
     {
-        // Maybe clone original...
-        return null;
+        Armour src = (Armour) original;
+
+        Armour copy = new Armour();
+        copy.setName(src.getName());
+        copy.setMaterial(src.getMaterial());
+        copy.setDurability(src.getDurability());
+        copy.setDefense(src.getDefense());
+        copy.setModifier(src.getModifier());
+        copy.setModifierLevel(src.getModifierLevel());
+        copy.setElement(src.getElement());
+
+        return copy;
     }
 }
